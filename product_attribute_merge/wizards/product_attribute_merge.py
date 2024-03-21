23f4daa743646e6e3fe72c9f83e824dbf0dbaf03
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2024-03-21 14:28:50 +0100

    fixup! Start applying changes and would be easier to use sql...

--- product_attribute_merge/wizards/product_attribute_merge.py
+++ product_attribute_merge/wizards/product_attribute_merge.py
@@ -58,30 +58,68 @@ class WizardProductAttributeMerge(models.TransientModel):
 
     @api.model
     def _merge_attribute_value(self, value_to_merge, value_merge_into):
+
         pass
 
     @api.model
     def _move_attribute_value(self, value, attribute):
-        for line in value.pav_attribute_line_ids:
-            for ptav in line.product_template_value_ids:
-                ptav.attribute_id = attribute
-            # __import__("pdb").set_trace()
-            # line.with_context(
-            #     update_product_template_attribute_values=False
-            # ).write({"value_ids": [(4, pav_replacement.id)]})
-            # FIXME this is not allowed :(
+        Ptal = self.env["product.template.attribute.line"]
+        Ptav = self.env["product.template.attribute.value"]
+        # ptal_to_delete = Ptal.browse()
+        ptal_to_move = value.pav_attribute_line_ids
+        for line in ptal_to_move:
+            # TODO handle archived line as well !
             line.with_context(
                 update_product_template_attribute_values=False
-            ).write({"attribute_id":  attribute.id})
-
-
-        # ptav = self.env["product.template.attribute.value"].search[
-        #     (
-        #     )
-        # ]
-        # todo - modify product_template_attribute_value
+            ).write({"value_ids": [(3, value.id)]})
 
         value.attribute_id = attribute
+
+        for line in ptal_to_move:
+            # Search for an existing line for attribute, if none create one
+            template = line.product_tmpl_id
+            ptav2move = line.product_template_value_ids.filtered(
+                lambda ptav: ptav.product_attribute_value_id == value
+            )
+            tpl_attr_line = template.attribute_line_ids.filtered(
+                lambda l: l.attribute_id == attribute
+            )
+            if not tpl_attr_line:
+                tpl_attr_line = Ptal.with_context(
+                    # Does not work at creation !
+                    update_product_template_attribute_values=False
+                ).create(
+                    {
+                        "product_tmpl_id": template.id,
+                        "attribute_id": attribute.id,
+                        "value_ids": [(6, False, [value.id])],
+                    }
+                )
+                # The create will add the value on all variant, clean it up
+                ptav_new = tpl_attr_line.product_template_value_ids.filtered(lambda ptav: ptav.product_attribute_value_id == value)
+                product_to_update = ptav2move.ptav_product_variant_ids
+                product_to_remove_value = ptav_new.ptav_product_variant_ids - product_to_update
+                product_to_remove_value.product_template_attribute_value_ids = [(3, ptav_new.id, 0)]
+            else:
+                # Add the new value to an existing line
+                product_to_update = ptav2move.ptav_product_variant_ids
+                tpl_attr_line.with_context(
+                    update_product_template_attribute_values=False
+                ).write({"value_ids": [(4, value.id)]})
+                ptav2move.write(
+                    {
+                        "attribute_line_id": tpl_attr_line.id
+                    }
+                )
+            # TODO cleanup ptal_to_move that are empty !
+
+
+    @api.model
+    def _delete_attribute_value(self, value):
+        if not value.pav_attribute_line_ids:
+            value.unlink()
+        else:
+            raise UserError(_(f"Deleting the attribute value {value.name} is not possible, because it is being used."))
 
     @api.model
     def _get_actions_for_values(self, attribute_to_merge, attribute_merge_into):
@@ -97,7 +135,7 @@ class WizardProductAttributeMerge(models.TransientModel):
                     "merge_into_attribute_id": attribute_merge_into.id,
                     "merge_into_attribute_value_id": False,
                 }
-                for value in attribute_to_merge.value_ids
+                # for value in attribute_to_merge.value_ids
             )
         return actions
 
@@ -109,7 +147,6 @@ class WizardProductAttributeMerge(models.TransientModel):
             elif action.attribute_value_action == "move":
                 self._move_attribute_value(action.attribute_value_id, action.merge_into_attribute_id)
                 pass
-            else:
-                pass
-
+            elif action.attribute_value_action == "delete":
+                self._delete_attribute_value(action.attribute_value_id)
         return
